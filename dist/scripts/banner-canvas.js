697cc83888461d7b97d480b62c5f11aba2cefdc6
omarashqar <o.ashqar@outlook.com>
2018-12-09 18:34:43 -0500

    new create-react-app

--- dist/scripts/banner-canvas.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var canvas = document.querySelector('.banner-canvas');
-canvas.width = canvas.parentElement.offsetWidth;
-canvas.height = canvas.parentElement.offsetHeight;
-var ctx = canvas.getContext('2d');
-
-ctx.fillRect(0, 0, canvas.width, canvas.height);
