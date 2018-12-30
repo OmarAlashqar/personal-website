a56af7e716b438da7b6cf99fff2794e0ef1d0eed
OmarAlashqar <o.ashqar@outlook.com>
2018-12-30 12:33:06 -0500

    build

---
+++ precache-manifest.a587ca25fe512672cc276963d351e131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ab29df4878f474545b5",
+    "url": "/static/js/main.2ab29df4.chunk.js"
+  },
+  {
+    "revision": "16c8866f5029132d614b",
+    "url": "/static/js/1.16c8866f.chunk.js"
+  },
+  {
+    "revision": "2ab29df4878f474545b5",
+    "url": "/static/css/main.c542e7c2.chunk.css"
+  },
+  {
+    "revision": "a9d8141d5b07c0aa1f607d5be319c3e8",
+    "url": "/index.html"
+  }
+];
