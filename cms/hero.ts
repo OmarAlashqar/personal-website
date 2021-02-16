5039d9e56103d9344a815f36416f830090ebd24a
Omar Alashqar <o.ashqar@outlook.com>
2021-02-16 12:57:48 -0500

    updated blurb

--- cms/hero.ts
+++ cms/hero.ts
@@ -2,9 +2,9 @@ export default {
   imageURL: `/moi.jpg`,
   title: `Hey 👋 I'm Omar Alashqar`,
   body: `
-  I'm into books, movies, and design. Wait, maybe I should lead with this instead:
+  I'm into bouldering, books, movies, and design. Wait, maybe I should lead with this instead:
   
   \\
-  I have experience with **software development**, **system architecture**, **cloud infrastructure**, **embedded systems**, and other things including making unnecessarily long lists like this one.
+  I'm a quick learner with experience in **software development**, **system architecture**, **cloud infrastructure**, **embedded systems**, and other things including making long lists like this one.
   `,
 };
